b8d4df1d0a224ff46b5c390f39a8884fae769733
tazaro20 <tapiguezazetrochinel@gmail.com>
2024-08-07 10:10:15 +0100

    projet test laravel et vue.js

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,6 +33,11 @@ const showingNavigationDropdown = ref(false);
                                     Dashboard
                                 </NavLink>
                             </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('students.index')" :active="route().current('students.index')||route().current('students.edit')||route().current('students.create')">
+                                    Students
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
